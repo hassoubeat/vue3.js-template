9413ca01a95b2d8e21f99e4ac6c4db3c6c6c29d4
hassoubeat <hassoubeat.work@gmail.com>
2022-10-03 13:32:42 +0900

    Support composition api on app.vue

--- src/js/components/app.vue
+++ src/js/components/app.vue
@@ -16,43 +16,12 @@
  
 <script>
 
-import { computed } from "vue";
-import { useStore } from 'vuex'
-import { useRouter } from 'vue-router';
-import { ROUTING_PATH } from "~/js/const";
+import { useEnhancer } from "./enhancer";
  
 export default {
   setup() {
-    const store = useStore();
-    const router = useRouter();
 
-    const isLogin = computed(() => {
-      return store.state.isLogin;
-    })
-
-    const login = () => {
-      store.commit("changeIsLogin", true);
-      console.log("loggined:" + store.state.isLogin);
-    };
-
-    const logout = () => {
-      store.commit("changeIsLogin", false);
-      console.log("loggined:" + store.state.isLogin);
-    };
-
-    const goBack = () => {
-      window.history.length > 1
-        ? router.go(-1)
-        : router.push('/')
-    }
-
-    return {
-      isLogin,
-      login,
-      logout,
-      goBack,
-      ROUTING_PATH
-    }
+    return useEnhancer();
   }
 }
 </script>
