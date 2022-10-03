596cb0db7c0134ccb3c0a5072bf4756b70c71992
hassoubeat <hassoubeat.work@gmail.com>
2022-10-03 10:44:20 +0900

    Update Routing pasth

--- src/js/const.js
+++ src/js/const.js
@@ -1,9 +1,9 @@
 export const ROUTING_PATH = {
-  TOP: '/html',
-  SUB: '/html/sub',
-  SUB_CHILD1: '/html/sub/children1',
-  SUB_CHILD2: '/html/sub/children2',
-  USER_LIST: '/html/users',
-  USER_DETAIL_IN_ID: '/html/users/:id',
-  USER_DETAIL_OUT_ID: '/html/users/'
+  TOP: '/',
+  SUB: '/sub',
+  SUB_CHILD1: '/sub/children1',
+  SUB_CHILD2: '/sub/children2',
+  USER_LIST: '/users',
+  USER_DETAIL_IN_ID: '/users/:id',
+  USER_DETAIL_OUT_ID: '/users/'
 };
